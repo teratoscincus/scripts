9027e12d93b89fca9b64e8727a5f4d4013789442
Max <teratoscincus@hotmail.com>
2023-05-05 11:24:07 +0200

    Add script to cd into dir and activate Pipenv

---
+++ cdpyvenv.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Change directory and activate Pipenv venv if a Pipfile is present.
+
+PIPFILE='Pipfile'
+
+cd "$1" || exit
+
+# Check for Pipenv venv
+if [[ -f "$PIPFILE" ]]; then
+	pipenv shell
+fi
