41b751ba99bd4e4794acbc8d71be2b35edbae46a
Max <teratoscincus@hotmail.com>
2023-05-26 09:47:35 +0200

    Rename, edit docstrings, and add var for filename

--- copylist.py
+++ copylist.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python3
 
 """
-A script to copy files and directories from $HOME to the CWD from which the script is
-called.
+A script to copy files and directories from users home directory to the CWD from which
+the script is called.
 
 Usage:
-    Reads in file and directory names to be copied from `.copy_list.txt` in current
+    Reads in file and directory names to be copied from `COPYLIST_FILE` in current
     working directory from where this script is called.
-    File and directories in `copy_list.txt` are expected to be paths relative to
-    $HOME.
+    File and directories listed in `COPYLIST_FILE` are expected to be paths relative to
+    the users home directory.
 
-    Ensure the directory containing this script is in PATH.
+    Ensure the directory containing this script is in PATH, for ease of use.
 
 Comment out lines in list:
-    Files and directories can be commented out using "#" in `.copy_list.txt`.
+    Files and directories can be commented out using "#" in `COPYLIST_FILE`.
 """
 
 import os
 import subprocess
 from pathlib import Path
 
+COPYLIST_FILE = ".copylist.txt"
+
 CWD = Path(os.getcwd())
 HOME = Path(os.path.expanduser("~"))
 
 # Output color formatting.
+B_RED = "\033[1;31m"
 B_GREEN = "\033[1;32m"
 B_BLUE = "\033[1;34m"
 COLOR_OFF = "\033[0m"
@@ -32,12 +35,11 @@ COLOR_OFF = "\033[0m"
 def copy_dirs(copy_list: list):
     for content in copy_list:
         content = content.rstrip("\n")
-        # Skip line commented out with "#" and blank lines.
         if content.startswith("#") or content == "":
             continue
         if not os.path.exists(HOME / content):
             continue
-        # Copy recursively to here.
+        # Copy recursively to CWD.
         print(f"Copying {content}")
         subprocess.run(["cp", "-rt", CWD, HOME / content])
         print(f"{B_GREEN}Copied{COLOR_OFF} {content}")
@@ -45,19 +47,18 @@ def copy_dirs(copy_list: list):
 
 
 if __name__ == "__main__":
-    with open(CWD / ".copy_list.txt", "r") as f:
+    with open(CWD / COPYLIST_FILE, "r") as f:
         copy_list = f.readlines()
 
     print(f"Destination: {B_BLUE}{CWD}{COLOR_OFF}")
     print("Files and directories to be copied:")
     for content in copy_list:
         content = content.rstrip("\n")
-        # Skip line commented out with "#" and blank lines.
         if content.startswith("#") or content == "":
             continue
         if not os.path.exists(HOME / content):
-            continue
-        print(HOME / content)
+            print(f"{B_RED}Not found: ~/{content}")
+        print(f"~/{content}")
     confirmation = input("\nProceed? (y/n) ").strip().lower()
 
     if confirmation == "y":
